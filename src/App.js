77fbec839582ade672ba05bb655769f809b9c588
aboameen230 <aboameen230@gmail.com>
2024-07-16 18:50:16 +0300

    home

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header/index";
-// import Home from "./Pages/Home/index";
+import Home from "./Pages/Home/index";
 import Footer from "./Components/Footer/index";
 import Contact from './Pages/Contact/index';
 import LogIn from './Pages/Log_in/index';
@@ -18,8 +18,8 @@ export default function App() {
         <Header />
         <Routes>
           <Route path="/">
-            {/* <Route index element={<Home />} />
-            <Route path="/Home" element={<Home />} /> */}
+            <Route index element={<Home />} />
+            <Route path="/Home" element={<Home />} />
             <Route path="/Log_in" element={<LogIn />} />
             <Route path="/Contact" element={<Contact />} />
             <Route path="/Sign_up" element={<Sign_up />} />
