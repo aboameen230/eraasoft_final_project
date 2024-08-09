d281c8972a4cdc5b715befc43a4e220b315f26c2
aboameen230 <aboameen230@gmail.com>
2024-08-10 02:49:07 +0300

    .

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -70,6 +70,55 @@ export default function Home() {
       });
   };
 
+  // const handleAddToCart = (product) => {
+  //   axios
+  //     .get("https://django-e-commerce-production.up.railway.app/carts/my-cart/", {
+  //       headers: {
+  //         Authorization: `Bearer ${window.localStorage.getItem("accessToken")}`,
+  //       },
+  //     })
+  //     .then((response) => {
+  //       const cart = response.data;
+
+  //       const productIncart = cart.some(
+  //         (cartItem) => cartItem.product.id === product.id
+  //       );
+
+  //       if (productIncart) {
+  //         console.log("Product already exists in the cart");
+  //       } else {
+  //         axios
+  //           .post(
+  //             "https://django-e-commerce-production.up.railway.app/carts/my-cart/",
+  //             {
+  //               product_id: product.id,
+  //               item_quantity: 1,
+  //             },
+  //             {
+  //               headers: {
+  //                 Authorization: `Bearer ${window.localStorage.getItem(
+  //                   "accessToken"
+  //                 )}`,
+  //               },
+  //             }
+  //           )
+  //           .then((response) => {
+  //             console.log("Product added to cart:", response.data);
+  //             alert("Added to cart successfully");
+  //           })
+  //           .catch((error) => {
+  //             console.error(
+  //               "There was an error adding the product to the cart!",
+  //               error
+  //             );
+  //           });
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error("There was an error fetching the cart!", error);
+  //     });
+  // };
+
   return (
     <div className="mt-28">
       <img src={banner} alt="Banner" className="bannerr" />
